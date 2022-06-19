560fe3820693d4156321368a8bfcacefbaf3cdfb
usrisavailable <65015199+usrisavailable@users.noreply.github.com>
2022-06-19 17:23:03 +0800

    Delete ch1 directory

--- ch1/ex6.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-main()
-{   
-    printf("%d\n", getchar() != EOF);
-    //return value 1
-}
--- ch1/ex7.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-main()
-{   
-    // the value of EOF is -1
-    printf("%d\n", EOF);
-}
