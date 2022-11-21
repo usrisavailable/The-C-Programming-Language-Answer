aa2f0e9c365dd26b68bdaaa374759fe15daa261f
usrisavailable <65015199+usrisavailable@users.noreply.github.com>
2022-11-21 20:12:02 +0800

    Create ex04.c

---
+++ ch03/ex04.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <ctype.h>
+
+#define debug(fmt, ...) fprintf(stderr, "line %d: " fmt "\n", __LINE__, __VA_ARGS__)
+#define MAX_LINE 1000
+
+void itoa(int n, char s[]);
+void reverse(char s[], int len);
+
+int main(int argc, char **argv)
+{
+    char s[MAX_LINE];
+    itoa(-65536, s);
+    debug("%s", s);
+    return 0;
+}
+
+void itoa(int n, char s[])
+{
+    unsigned num;//double is better ,but % is not supprot
+    int sign = 1;
+    int i = 0;
+    if (n < 0)
+    {
+        num = -n;
+        sign = -1;
+    }
+    else
+        num = n;
+
+    do{
+        s[i++] = num % 10 + '0';
+        num = num / 10;
+    }while(num);
+
+    if (sign == -1)
+    {
+        s[i++] = '-';
+    }
+    s[i] = '\0';
+    reverse(s, i);
+    return;
+}
+
+void reverse(char s[], int len)
+{
+    int i = 0;
+    for (i; i < len / 2; i++)
+    {
+        int temp;
+        temp = s[i];
+        s[i] = s[len - i -1];
+        s[len - i -1] = temp;
+    }
+    
+    return;
+}
